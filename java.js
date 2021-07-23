138d5fc2133cc00f57acdc39a3e0b0a2f846dd7a
unknown <wesa@wesam_ali1.Cairo.TelecomEgypt.corp>
2021-07-23 22:13:34 +0200

    first commit

---
+++ java.js
@@ -0,0 +1,62 @@
+
+//***********************Task 1***************************** */
+
+// a- '9' - '5'; // result is 4
+// b- '19' - '13' + '17'; // the result is 617
+// c- '19' - '13' + 17; // the result is 23
+// d- '123' < 57; // the result is False
+// e- 5 + 6 + '4' + 9 - 4 - 2; 
+//    11 + '4' + 3  // the result is 114 + 3 = 117
+
+//************************Task 2*************************** */
+
+// let lang = window.prompt("Please enter your Language ");
+// switch (lang) {
+//     case "chinese" :
+//     case "mandarin" :
+//         console.log('MOST number of native speakers!')
+//         break ;
+//     case "spanish" :
+//         console.log('2nd place in number of native speakers')
+//         break ;
+//     case "english" :
+//         console.log('3rd place')
+//         break ;
+//     case "hindi" :
+//         console.log('Number 4')
+//         break;
+//     case "arabic" :
+//         console.log('5th most spoken language')
+//         break;
+//     default:
+//         console.log('Great language too') 
+// }
+
+//***************************Task 3 ***************************** *
+
+// function describeCountry(country,pop,capitalCity ) {
+//     return `${country} has ${pop} Million People and its capital city  is ${capitalCity}  `
+// }
+
+// let theFirstContry = describeCountry(prompt("What is  your Country ?"),
+//                         prompt("What is  the Population of this Country ?"),
+//                         prompt("What is the Capital city of this Country ? "))
+// console.log(theFirstContry)
+
+// let theSecondCountry = describeCountry(prompt("What is  your Country ?"),
+//                         prompt("What is  the Population of this Country ?"),
+//                         prompt("What is the Capital city of this Country ? "))
+// console.log(theSecondCountry)
+
+// let theThirdCountry = describeCountry(prompt("What is  your Country ?"),
+//                         prompt("What is  the Population of this Country ?"),
+//                         prompt("What is the Capital city of this Country ? "))
+// console.log(theThirdCountry)
+
+
+//*****************************Task 4 **************************** */
+
+// let Bill = Number(prompt("Please enter the bill value :"));
+
+// let Tip = (Bill >= 50 && Bill <= 300)? Bill * 0.15 : Bill * 0.2
+// console.log(`The bill is ${Bill}, the tip is ${Tip}, and the total value is ${Bill + Tip}`)
